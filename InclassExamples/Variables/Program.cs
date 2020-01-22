0ecd741640807760b71ddfca75d9c8d0f3e3b0c7
kudh0000 <kudh0000@PCB-LAB360-09>
2020-01-22 12:55:50 -0600

    Created Variables

--- InclassExamples/Variables/Program.cs
+++ InclassExamples/Variables/Program.cs
@@ -11,6 +11,27 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create integer name string firstname
+            string firstName;
+
+
+            //Assign value
+            firstName = "Arthur";
+
+            //Create int
+            int favoriteNumber = 25;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Kudhlande";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber
+               + "\n Which equates to a crazy number of " + crazyNumber);
+
+
+            Console.ReadKey();
+
         }
     }
 }
