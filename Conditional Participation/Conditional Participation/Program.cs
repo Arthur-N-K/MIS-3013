309c9a70df047fec5d96c9bf76dd9d5aa20b8095
kudh0000 <kudh0000@PCB-LAB360-09>
2020-01-29 13:15:39 -0600

    good

---
+++ Conditional Participation/Conditional Participation/Program.cs
@@ -0,0 +1,49 @@
+﻿//Arthur Kudhlande
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Participation
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Pick Heads or Tails >>");
+            string CointossAs = Console.ReadLine();
+            
+            //int Cointoss  = ;
+
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 2);
+            string outcome = " ";
+
+            if (randomNumber == 0)
+            {
+                outcome = "Heads";
+            }
+            else if (randomNumber == 1)
+            {
+                outcome = "Tails";
+            }
+
+            if (CointossAs==outcome)
+            {
+                Console.WriteLine("Righhtt");
+            }
+            else if (CointossAs != outcome)
+                    {
+                Console.WriteLine("Wrong!!");
+                
+            }
+
+
+            const string Name = "Arthur";
+            Console.WriteLine(Name);
+            Console.ReadKey();
+        }
+    }
+}
