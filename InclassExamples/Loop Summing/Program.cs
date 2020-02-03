fd62a34d5cd48485c848dd60581c686695d0b4ca
kudh0000 <kudh0000@PCB-LAB360-09>
2020-02-03 13:08:33 -0600

    First

---
+++ InclassExamples/Loop Summing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which number do you want to count to?>>");
+            int threshold = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= threshold; i++)
+            {
+                Console.Write(i + " ");
+
+            }
+
+            double sum = 0;
+            string answer;
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine("Please give a number to add>>");
+                double number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+
+                Console.WriteLine("Another number? (y/n) >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() =="y");
+
+            Console.WriteLine($"Your summation of values is {sum.ToString("N2")}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
--- Loop coin toss/Loop coin toss/Program.cs
+++ Loop coin toss/Loop coin toss/Program.cs
@@ -10,6 +10,60 @@ namespace Loop_coin_toss
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Pick Heads or Tails >>");
+            string CointossAs = Console.ReadLine();
+            int cointoss = Convert.ToInt32(CointossAs);
+            int cointoss;
+
+            Random rand = new Random();
+            int randomNumber = 0;
+            while (CointossAs != randomNumber)
+            {
+                int randomNumber = rand.Next(0, 2);
+                string outcome = " ";
+
+                if (randomNumber == 0)
+                {
+                    outcome = "Heads";
+                }
+                else if (randomNumber == 1)
+                {
+                    outcome = "Tails";
+                }
+
+                if (CointossAs == outcome)
+                {
+                    Console.WriteLine("Righhtt");
+                }
+                else if (CointossAs != outcome)
+                {
+                    Console.WriteLine("Wrong!!");
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid statement");
+                    Console.ReadKey();
+                    return;
+
+                }
+
+            }
+
+
+
+
+
+            const string Name = "Arthur";
+            Console.WriteLine(Name);
+            Console.ReadKey();
         }
+
+
+
+
+
+
+    }
     }
 }
