f83fb73fbf5305ea2ad708f4cbc682df39711776
kudh0000 <kudh0000@PCB-LAB360-09>
2020-02-12 12:11:15 -0600

    First test

---
+++ Conditional Participation/String Manupilation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manupilation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning ");
+
+            Console.WriteLine(" What word would you want to change? ");
+            Console.ReadLine();
+
+            Console.WriteLine("What word would you want to look for? ");
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
