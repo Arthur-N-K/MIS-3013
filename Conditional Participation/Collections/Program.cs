7136bf1a94d173f893899ba3675ca22c5514ec56
kudh0000 <kudh0000@PCB-LAB360-09>
2020-02-19 12:25:01 -0600

    First loop

---
+++ Conditional Participation/Collections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exam;
+
+            do
+            {
+                Console.WriteLine("Input your exam score >>");
+                double examS = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Do you have another exam to add? (yes/no)");
+                exam = Console.ReadLine();
+            } while (exam == "yes");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
