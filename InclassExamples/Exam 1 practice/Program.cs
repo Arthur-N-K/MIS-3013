6ad0dba8d16d2b72be6f1fa5fab38ede31846e26
kudh0000 <kudh0000@PCB-LAB360-09>
2020-02-24 13:06:58 -0600

    truy

---
+++ InclassExamples/Exam 1 practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_1_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Input your word or your phrase");
+            string userword = Console.ReadLine();
+            int index = userword.Length;
+
+            for (int i = userword.Length -1; i >= 0; i--)
+            {
+                string newword = userword[i];
+                Console.WriteLine(newword);
+
+                do
+                {
+                    Console.WriteLine("Your phrase is a palindrome!!");
+                } while (userword[0] == newword[0]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
