d961e12b1a1ce8975659ce0c590fef117a56eb8f
kudh0000 <kudh0000@PCB-LAB360-09>
2020-03-02 13:06:16 -0600

    ltes2

--- Conditional Participation/Collections2/Program.cs
+++ Conditional Participation/Collections2/Program.cs
@@ -10,18 +10,67 @@ namespace Collections2
     {
         static void Main(string[] args)
         {
-            int[] Coursenumbers = new int[3];
-            string[] Coursenames = { Console.ReadLine()};
+            string[] Coursenumbers = new string[3];
+            string[] Coursenames = new string [3];
+           
+
+           
+
+            for (int i = 0; i < Coursenames.Length; i++)
+            {
+                Console.WriteLine("Input your subject name");
+                Coursenames[i] = Console.ReadLine();
+            }
+
+
+
+            for (int i = 0; i < Coursenumbers.Length; i++)
+            {
+                Console.WriteLine(" Input your course number");
+                Coursenumbers[i] = Console.ReadLine();
+                
+            }
+
+            for (int i = 0; i < Coursenumbers.Length; i++)
+            {
+                Console.WriteLine($" Your course is {Coursenames[i].ToUpper()} {Coursenumbers[i].ToUpper()}");
+            }
+
+
+            //Problem 2
+            List<double> grades = new List<double>();
+
+            string examinput;
 
             do
             {
-                Console.WriteLine("Input your subject name");
-                Coursenames[0] = Console.ReadLine();
-                Console.WriteLine("Another subject? (yes/no)");
-                string answer = Console.ReadLine();
+                Console.WriteLine("Input your exam score >>");
+                double examS = Convert.ToDouble(Console.ReadLine());
 
-            } while (answ);
 
+                grades.Add(examS);
+
+                //double max;
+
+
+                Console.WriteLine("Do you have another exam to add? (yes/no)");
+                examinput = Console.ReadLine();
+            } while (examinput == "yes");
+            
+
+            double sum = 0;
+           
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                double grade = grades[i];
+                sum = sum + grade;
+               
+                }
+            
+            double average = sum / grades.Count;
+
+            Console.WriteLine($"Your average for the {grades.Count} exam  grade is {average.ToString("N3")}");
 
 
 
