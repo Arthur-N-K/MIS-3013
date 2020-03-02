65f2aef7f1e2e324710eed775a483832e18b0fbf
kudh0000 <kudh0000@PCB-LAB360-09>
2020-03-02 12:34:30 -0600

    one

--- Conditional Participation/Collections/Program.cs
+++ Conditional Participation/Collections/Program.cs
@@ -23,18 +23,38 @@ namespace Collections
                grades.Add(examS);
 
                 //double max;
-                for (int i = 0; i < examS; i++)
-                {
-                    Console.WriteLine(i);
-                }
+                
 
                 Console.WriteLine("Do you have another exam to add? (yes/no)");
                 examinput = Console.ReadLine();
             } while (examinput == "yes");
             ;
 
+            double sum = 0;
+            double min = grades[0];
+            double max = grades[0];
 
-           
+            for (int i = 0; i < grades.Count ; i++)
+            {
+                double grade = grades[i];
+                sum = sum + grade;
+
+                if (grade < min)
+                {
+                    min = grade;
+                }
+
+                if (grade > max)
+                {
+                    max = grade;
+                }
+            }
+            double average = sum / grades.Count;
+
+            Console.WriteLine($"Your average for the {grades.Count} exam  grade is {average.ToString("N3")}");
+            Console.WriteLine($"Your minimum exam  grade is {min.ToString("N3")}");
+            Console.WriteLine($"Your maximum exam  grade is {max.ToString("N3")}");
+
             Console.ReadKey();
         }
     }
---
+++ Conditional Participation/Collections2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Coursenumbers = new int[3];
+            string[] Coursenames = { Console.ReadLine()};
+
+            do
+            {
+                Console.WriteLine("Input your subject name");
+                Coursenames[0] = Console.ReadLine();
+                Console.WriteLine("Another subject? (yes/no)");
+                string answer = Console.ReadLine();
+
+            } while (answ);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
